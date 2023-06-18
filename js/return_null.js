090ebab341c3be45a81e63f6ba95b0c2ffa77bf4
dmitriyTmax <tambovtsev.dmitriy@gmail.com>
2023-06-18 22:50:17 +0300

    modify return_null.js get 0 immidiatly and remove color

--- js/return_null.js
+++ js/return_null.js
@@ -1,28 +1,18 @@
-function animateSpeedometerValue(startValue, endValue, duration) {
-  const labelElement = document.querySelector('#speedometer-label');
-  const range = endValue - startValue;
-  const startTime = performance.now();
-
-  function updateSpeedometerLabel(currentTime) {
-    const elapsedTime = currentTime - startTime;
-    if (elapsedTime >= duration) {
-      // Animation completed
-      labelElement.textContent = endValue.toFixed(2);
-    } else {
-      const progress = elapsedTime / duration;
-      const currentValue = startValue + range * progress;
-      labelElement.textContent = currentValue.toFixed(2);
-      window.requestAnimationFrame(updateSpeedometerLabel);
-    }
+document.addEventListener("DOMContentLoaded", function() {
+  var startBtn = document.querySelector(".start-btn");
+  if (startBtn) {
+    startBtn.addEventListener("click", function() {
+      setTimeout(function() {
+        var speedometerLabel = document.getElementById("speedometer-label");
+        if (speedometerLabel) {
+          speedometerLabel.innerText = "0.00";
+        }
+        
+        var svgTicks = document.querySelectorAll(".svg-tick");
+        svgTicks.forEach(function(path) {
+          path.classList.remove("color");
+        });
+      }, 19000); // 19 seconds delay (19,000 milliseconds)
+    });
   }
-
-  window.requestAnimationFrame(updateSpeedometerLabel);
-}
-
-// Обробник події натискання на кнопку "start-btn"
-document.querySelector('.start-btn').addEventListener('click', () => {
-  // Затримка в 10 секунд перед виконанням функції
-  setTimeout(() => {
-    animateSpeedometerValue(0.00, 0.00, 1000);
-  }, 18000);
-}); 
+});
