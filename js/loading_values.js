2c04011751b7df087cf0c23db659f3726a981b6b
dmitriyTmax <tambovtsev.dmitriy@gmail.com>
2023-06-18 17:32:52 +0300

    Solve error in loading_values.js

--- js/loading_values.js
+++ js/loading_values.js
@@ -1,25 +1,27 @@
-// Find all elements with the classes .card__trait and .loading
-const cardTraits = document.querySelectorAll('.card__trait');
-const loadings = document.querySelectorAll('.loading');
+(function() {
+  // Find all elements with the classes .card__trait and .loading
+  const cardTraits = document.querySelectorAll('.card__trait');
+  const loadings = document.querySelectorAll('.loading');
 
-// Function to hide .card__trait elements and show .loading elements
-function hideTraitsAndShowLoadings() {
-  // Hide all .card__trait elements
-  cardTraits.forEach((cardTrait) => {
-    cardTrait.classList.add('hide');
-  });
+  // Function to hide .card__trait elements and show .loading elements
+  function hideTraitsAndShowLoadings() {
+    // Hide all .card__trait elements
+    cardTraits.forEach((cardTrait) => {
+      cardTrait.classList.add('hide');
+    });
 
-  // Show all .loading elements
-  loadings.forEach((loading) => {
-    loading.classList.remove('hide');
-  });
-}
+    // Show all .loading elements
+    loadings.forEach((loading) => {
+      loading.classList.remove('hide');
+    });
+  }
 
-// Add event listener to all elements with the class .start-btn
-const startBtns = document.querySelectorAll('.start-btn');
-startBtns.forEach((startBtn) => {
-  startBtn.addEventListener('click', () => {
-    // Call the hideTraitsAndShowLoadings function when .start-btn is clicked
-    hideTraitsAndShowLoadings();
+  // Add event listener to all elements with the class .start-btn
+  const startBtns = document.querySelectorAll('.start-btn');
+  startBtns.forEach((startBtn) => {
+    startBtn.addEventListener('click', () => {
+      // Call the hideTraitsAndShowLoadings function when .start-btn is clicked
+      hideTraitsAndShowLoadings();
+    });
   });
-});
+})();
