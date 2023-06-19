305d7f9da6b0e46ff0831ae82c785fb634ebe0b2
dmitriyTmax <tambovtsev.dmitriy@gmail.com>
2023-06-19 16:03:56 +0300

    Smoosty changes on speedometer

--- js/change_color_speedometer.js
+++ js/change_color_speedometer.js
@@ -1,9 +1,29 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   var startBtn = document.querySelector("#start-btn");
+//   if (startBtn) {
+//     startBtn.addEventListener("click", function() {
+//       var svgTicks = document.querySelectorAll(".svg-tick");
+//       svgTicks.forEach(function(tick) {
+//         tick.setAttribute("stroke", "#eeeeef");
+//       });
+
+//       setTimeout(function() {
+//         svgTicks.forEach(function(tick) {
+//           tick.setAttribute("stroke", "#2E7F98");
+//         });
+//       }, 20000); // 20 секунд (20 000 мілісекунд)
+//     });
+//   }
+// });
+
+// --- Smoosly changes on sppedomert --- //
 document.addEventListener("DOMContentLoaded", function() {
   var startBtn = document.querySelector("#start-btn");
   if (startBtn) {
     startBtn.addEventListener("click", function() {
       var svgTicks = document.querySelectorAll(".svg-tick");
       svgTicks.forEach(function(tick) {
+        tick.style.transition = "stroke 0.5s ease-in-out";
         tick.setAttribute("stroke", "#eeeeef");
       });
 
@@ -11,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
         svgTicks.forEach(function(tick) {
           tick.setAttribute("stroke", "#2E7F98");
         });
-      }, 20000); // 20 секунд (20 000 мілісекунд)
+      },19500); // 20 seconds (20,000 milliseconds)
     });
   }
 });
